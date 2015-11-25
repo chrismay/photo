2347aa14ced1eef41c7aaa2ab66f85f4f7aed5df
Chris May <chris.may@blackpepper.co.uk>
2015-11-25 10:40:05 +0000

    client pom runs grunt

--- web/client/src/main/resources/public/Gruntfile.js
+++ web/client/src/main/resources/public/Gruntfile.js
@@ -1,117 +1,102 @@
 'use strict';
 
-module.exports = function (grunt) {
+module.exports = function(grunt) {
 
-    // Load grunt tasks automatically
-    require('load-grunt-tasks')(grunt);
-    // Time how long tasks take. Can help when optimizing build times
-    require('time-grunt')(grunt);
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
 
-    // Configurable paths for the application
-    var appConfig = {
-        app: 'app'
-    };
-    grunt.loadNpmTasks('grunt-connect-proxy'); // Proxy support
+  // Configurable paths for the application
+  var appConfig = {
+    app: 'app'
+  };
+  grunt.loadNpmTasks('grunt-connect-proxy'); // Proxy support
 
 
-    grunt.initConfig({
+  grunt.initConfig({
 
-        appConfig: appConfig,
+    appConfig: appConfig,
 
-        // Protractor settings
-        protractor: {
-            all: {
-                options: {
-                    configFile: "e2e-tests/protractor-conf.js",
-                }
-            },
-        },
-        karma: {
-            unit: {
-                configFile: 'karma.conf.js',
-                singleRun:true
-            },
-            watch: {
-                configFile: 'karma.conf.js'
-            }
+    // Protractor settings
+    protractor: {
+      all: {
+        options: {
+          configFile: "e2e-tests/protractor-conf.js",
+        }
+      },
+    },
+    karma: {
+      options: {
+        configFile: 'karma.conf.js',
+      },
+      unit: {
+        singleRun: true
+      },
+      watch: {
+        singleRun: false
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          base: appConfig.app,
+          keepalive: true,
+          port: 9000,
+          middleware: function(connect, options) {
+            var proxy = require('grunt-connect-proxy/lib/utils').proxyRequest;
+            return [
+              // Include the proxy first
+              proxy,
+              connect.static(options.base),
+              connect.directory(options.base)
+            ];
+          }
+        }
+      },
+      proxies: [{
+        context: '/competition',
+        host: 'localhost',
+        port: 8080,
+        https: false,
+        xforward: false,
+        hideHeaders: ['x-removed-header']
+      }, {
+        context: '/photo',
+        host: 'localhost',
+        port: 8080,
+        https: false,
+        xforward: false,
+        hideHeaders: ['x-removed-header']
+      }]
+    },
 
-        },
-        connect: {
-            server: {
-                options: {
-                    base: appConfig.app,
-                    keepalive: true,
-                    port: 9000,
-                    middleware: function (connect, options) {
-                        var proxy = require('grunt-connect-proxy/lib/utils').proxyRequest;
-                        return [
-                            // Include the proxy first
-                            proxy,
-                            connect.static(options.base),
-                            connect.directory(options.base)
-                        ];
-                    }
-                }
-            },
-            proxies: [
-                {
-                    context: '/competition',
-                    host: 'localhost',
-                    port: 8080,
-                    https: false,
-                    xforward: false,
-                    hideHeaders: ['x-removed-header']
-                },
-                {
-                    context: '/photo',
-                    host: 'localhost',
-                    port: 8080,
-                    https: false,
-                    xforward: false,
-                    hideHeaders: ['x-removed-header']
-                }
-            ]
-        },
+    jshint: {
+      prod: {
+        src: [
+          'Gruntfile.js',
+          'app/current_competition/*.js',
+          'app/components/*.js'
+        ],
+        options: {
+          jshintrc: './.jshintrc'
+        }
+      },
+      test: {
+        src: ['e2e-tests/**/*.js'],
+        options: {
+          jshintrc: 'e2e-tests/.jshintrc'
+        }
+      }
+    }
+  });
 
-        jshint: {
-            prod: {
-                src: [
-                    'Gruntfile.js',
-                    'app/current_competition/*.js',
-                    'app/components/*.js'
-                ],
-                options: {
-                    jshintrc: './.jshintrc'
-                }
-            },
-            test: {
-                src: ['e2e-tests/**/*.js'],
-                options: {
-                    jshintrc: 'e2e-tests/.jshintrc'
-                }
-            }
-        }/*,
+  grunt.registerTask('serve', [
+    'configureProxies:server',
+    'connect:server'
+  ]);
 
-         exec: {
-         build_style: {
-         command: "npm install",
-         cwd: "./style"
-         }
-         /*,
-         copy_style: {
-         command: "cp -r fonts dist js less ../src/main/resources/static/",
-         cwd: "./style"
-         }
-         }*/
+  grunt.registerTask('e2e', ['selenium_start', 'protractor', 'selenium_stop']);
 
-    });
-
-    grunt.registerTask('serve', [
-        'configureProxies:server',
-        'connect:server'
-    ]);
-
-    grunt.registerTask('e2e', ['selenium_start', 'protractor', 'selenium_stop']);
-
-    grunt.registerTask('default', ['jshint', 'exec']);
+  grunt.registerTask('default', ['jshint']);
 };
